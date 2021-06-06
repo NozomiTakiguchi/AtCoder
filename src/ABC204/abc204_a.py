4fc648a2093b3a2eef86b97e51ecc6eaf438ee1e
NozomiTakiguchi <takiguchi19960122@gmail.com>
2021-06-06 23:00:32 +0900

    tweak

--- src/ABC204/abc204_a.py
+++ src/ABC204/abc204_a.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     x,y = map(int, input().split())
     if x != y:
         ans = [i for i in [0, 1, 2] if i not in [x, y]]
-        print(ans[0])
+        print(*ans)
     else:
         print(x)
