7a2aa7429bbb77027ce4c12970c9785a15d1d632
NozomiTakiguchi <takiguchi19960122@gmail.com>
2021-06-07 00:04:15 +0900

    impl dfs

--- src/ABC204/abc204_c.py
+++ src/ABC204/abc204_c.py
@@ -1,37 +1,45 @@
 import sys
-input = sys.stdin.readline
 
-n, m = [int(x) for x in input().split()] # nは頂点の数、mは辺の数
-g = [[] for _ in range(n+1)] # 隣接リスト
+# 再帰の最大回数を増やしておくことで、 AC になるパターンが多いらしい
+# 再帰の回数をデフォルトのまま (1000) にしておくと Runtime Error(Stack Over Flow) になることがあるらしい
+sys.setrecursionlimit(10000)
 
-_a = []
-for _ in range(m):
-    a, b = [int(x) for x in input().split()]
-    _a.append(a)
-    g[a].append(b)
+def dfs(start_point: int, mapping: list, _result: list, avoid_duplicate=True):
+    """ 深さ優先探索アルゴリズム
 
-if len(_a) == 0:
-    print(n)
-    exit()
+    Args:
+        start_point (int): 経路を検証したい頂点. 
+        mapping (list): 頂点と,その隣接する頂点のリスト. それぞれインデックスが要素として入っている (頂点 2 と隣接する頂点が 4 のとき、リストには [[..], [3], [..],[..]] と入っている)
+        _result (list): 初期値は [False] * 頂点数のリスト. 再帰的に参照する.
+        avoid_duplicate (bool, optional): 同じ頂点を二度以上調べたい場合は False. たぶん False にすることはない.
+    """
+    if _result[start_point]:
+        if avoid_duplicate:
+            return 
 
-from collections import deque
+    # 自分自身と繋がっている
+    _result[start_point] = True
+    for v in mapping[start_point]: # 渡された頂点と隣接する頂点に関して調べる
+        dfs(v, mapping, _result)
 
-def bfs(u):
-    cnt = 0
-    queue = deque([u])
-    d = [None] * (n+1) # uからの距離の初期化
-    d[u] = 0 # 自分との距離は0
-    while queue:
-        cnt += 1
-        v = queue.popleft()
-        for i in g[v]:
-            if d[i] is None:
-                d[i] = d[v] + 1
-                queue.append(i)
-    return cnt
+def run(n, m):
+    _l = [[] for _ in range(n)]
+    # _l[i] は、都市 i から道路で直接つながっている都市のリスト 
+    for _ in range(m):
+        a, b = map(int, input().split())
+        _l[a-1].append(b-1)
 
-# 0からの各頂点の距離
-ans = 0
-for i in _a:
-    ans += bfs(i)
-print(ans)
+    ans = 0
+    for i in range(n):
+        result = [False]*n
+        dfs(i, _l, result)
+        # print(f'mapping is {_l}')
+        # print(f'city {i+1} is connected to {result}')
+        ans += sum(result)
+    print(ans)
+
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    run(n, m)
